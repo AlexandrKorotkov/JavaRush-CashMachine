9318ffbecf2149481413b091e50642b8ab0dad71
AlexandrKorotkov <91676213+AlexandrKorotkov@users.noreply.github.com>
2021-10-19 15:13:31 +0300

    task02: 1. Добавлены 2 кастомных исключения в пакет exception. 2. Добавлены методы в класс ConsoleHelper для чтения и вывода в консоль.

--- src/task2613/ConsoleHelper.java
+++ src/task2613/ConsoleHelper.java
@@ -1,4 +1,25 @@
 package task2613;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+
+ */
 public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String in="";
+        try {
+            in= bis.readLine();
+        } catch (IOException e) {
+        }
+        return in;
+    }
 }
