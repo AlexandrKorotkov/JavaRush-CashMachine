506e14198aa48c05ba3c1d3e92cd2ab28c2cd17f
AlexandrKorotkov <91676213+AlexandrKorotkov@users.noreply.github.com>
2021-10-20 17:05:14 +0300

    task10: 1.Реализован механизм выдачи купюр WithdrawCommand жадным алгоритмом. 2. Добавлен метод снятия со счета в CurrencyManipulator.

--- src/task2613/CurrencyManipulator.java
+++ src/task2613/CurrencyManipulator.java
@@ -1,7 +1,8 @@
 package task2613;
 
-import java.util.HashMap;
-import java.util.Map;
+import task2613.exception.NotEnoughMoneyException;
+
+import java.util.*;
 
 /*
 Класс хранит информацию про выбранную валюту.
@@ -33,14 +34,46 @@ public class CurrencyManipulator {
     Считает общую сумму для указанной валюты.
      */
     public int getTotalAmount() {
-        int sum=0;
+        int sum = 0;
         for (Map.Entry<Integer, Integer> pair : denominations.entrySet()) {
-            sum+=pair.getKey()*pair.getValue();
+            sum += pair.getKey() * pair.getValue();
         }
         return sum;
     }
 
-    public boolean hasMoney(){
+    public boolean hasMoney() {
         return !denominations.isEmpty();
+    }
+
+    public boolean isAmountAvailable(int expectedAmount) {
+        return getTotalAmount() >= expectedAmount;
+    }
+/*
+Снятие со счета жадным алгоритмом.
+ */
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException {
+        int sum = expectedAmount;//будет хранить оставшуюся сумму, которую надо довыдать.
+        Map<Integer, Integer> denominationsCopy = new HashMap<>(denominations);
+        List<Integer> keys = new ArrayList<>(denominationsCopy.keySet());
+        Collections.sort(keys);
+        Collections.reverse(keys);//теперь номиналы идут по убыванию
+        Map<Integer, Integer> res = new TreeMap<>(Comparator.reverseOrder());
+        for (Integer den : keys) {
+            int key = den;
+            int value = denominationsCopy.get(key);
+            while (true) {
+                if (sum < key || value == 0) {
+                    denominationsCopy.put(key, value);
+                    break;
+                }
+                sum -= key;
+                value--;
+                if (res.containsKey(key)) res.put(key, res.get(key) + 1);
+                else res.put(key, 1);
+            }
+        }
+        if (sum > 0) throw new NotEnoughMoneyException();
+        else denominations = denominationsCopy;
+        return res;
     }
 }
--- src/task2613/command/WithdrawCommand.java
+++ src/task2613/command/WithdrawCommand.java
@@ -1,8 +1,44 @@
 package task2613.command;
 
- class WithdrawCommand  implements Command{
-    @Override
-    public void execute() {
+import task2613.ConsoleHelper;
+import task2613.CurrencyManipulator;
+import task2613.CurrencyManipulatorFactory;
+import task2613.exception.InterruptOperationException;
+import task2613.exception.NotEnoughMoneyException;
 
+import java.util.Map;
+
+class WithdrawCommand implements Command {
+
+    @Override
+    public void execute() throws InterruptOperationException {
+        String code = ConsoleHelper.askCurrencyCode();
+        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(code);
+        while (true) {
+            ConsoleHelper.writeMessage("Введите сумму: ");
+            String expectedAmountStr = ConsoleHelper.readString();
+            if (expectedAmountStr == null) {
+                ConsoleHelper.writeMessage("Введена некорректная сумма. Повторите");
+            } else {
+                try {
+                    int expectedAmount = Integer.parseInt(expectedAmountStr);
+                    if (!manipulator.isAmountAvailable(expectedAmount)) {
+                        ConsoleHelper.writeMessage("Недостаточная сумма на счете. Повторите.");
+                    } else {
+                        Map<Integer, Integer> map = manipulator.withdrawAmount(expectedAmount);
+                        for (Map.Entry<Integer, Integer> pair : map.entrySet()) {
+                            ConsoleHelper.writeMessage("\t" + pair.getKey() + " - " + pair.getValue());
+                        }
+                        ConsoleHelper.writeMessage("Транзакция проведена успешно.");
+                        break;
+                    }
+                } catch (NotEnoughMoneyException e) {
+                    ConsoleHelper.writeMessage("Недостаточно банкнот для выдачи указанной суммы.Введите другую сумму.");
+                } catch (NumberFormatException e) {
+                    ConsoleHelper.writeMessage("Введена некорректная сумма");
+                }
+            }
+        }
     }
 }
+
